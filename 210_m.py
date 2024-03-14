4c17e908b2dc3c0452c967b36e0a304af5432b6d
Sarmat Totikov <90254273+TORTIKOV@users.noreply.github.com>
2024-03-14 17:03:41 +0300

    Create 210_m.py

---
+++ 210_m.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+
+        visited = [0] * numCourses
+        order = []
+
+        def visit(vertex):
+            visited[vertex] = -1
+            for v in graph[vertex]:
+                if visited[v] == 0:
+                    if not visit(v):
+                        return False
+                if visited[v] == -1:
+                    return False
+            visited[vertex] = 1
+            order.append(vertex)
+            return True
+
+        for vertex in range(numCourses):
+            if visited[vertex] == 0:
+                if not visit(vertex):
+                    return []
+
+        return order[::-1]
